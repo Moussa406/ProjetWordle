86924ab4c53a8b38b8f019ff886be33e2f672b96
Seb-Prod <sebastien.drillaud@gmail.com>
2024-12-04 09:38:25 +0100

    ajout listener clavier

--- js/jeu.js
+++ js/jeu.js
@@ -3,6 +3,8 @@ creationGrille();
 
 let ligne = 0;
 let colone = 0;
+let motMystere = "salut";
+let motSaisie = "";
 
 function setCouleurCase() {
   if (colone < 5) {
@@ -23,17 +25,59 @@ const grille = document.getElementById("grille");
 grille.addEventListener("keydown", (event) => {
   const regex = /^[a-zA-Z]$/;
 
-  if (event.key === "Enter") {
-    // grille.style.backgroundColor = 'lightblue';
+  if (event.key === "Enter" && colone === 5) {
+    motExiste(motSaisie)
+      ? checkCorespondance(motSaisie)
+      : console.log("existe pas");
   }
 
   if (regex.test(event.key)) {
     if (colone < 5) {
       const maCase = document.getElementById(`l${ligne}c${colone}`);
       maCase.textContent = event.key;
+      motSaisie = motSaisie + event.key;
+      //
       colone++;
       setCouleurCase();
-      console.log(colone);
     }
   }
 });
+
+function checkCorespondance(mot) {
+  const tableauMotSaisie = mot.split("");
+  const tableauMotMystere = motMystere.split("");
+
+  for (let i = 0; i < tableauMotSaisie.length; i++) {
+    console.log(i);
+    if (tableauMotMystere[i] === tableauMotSaisie[i]) {
+      console.log(`${tableauMotMystere[i]} === ${tableauMotSaisie[i]}`);
+      console.log(tableauMotSaisie[i] + " -> ok");
+      couleurCase(i, 2)
+    } else {
+      tableauMotMystere.includes(tableauMotSaisie[i])
+        ? couleurCase(i,1)
+        : couleurCase(i,0);
+    }
+  }
+}
+
+function motExiste(mot) {
+  return true;
+}
+
+function couleurCase(maColone, point) {
+  const maCase = document.getElementById(`l${ligne}c${maColone}`);
+  switch (point) {
+    case 2:
+      maCase.classList.toggle("lettreOK");
+      break;
+
+    case 1:
+        maCase.classList.toggle("lettreExiste");
+      break;
+
+    default:
+        maCase.classList.toggle("lettreKO");
+      break;
+  }
+}
