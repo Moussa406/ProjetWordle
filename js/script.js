5e4c9176041af6d548bed22015964866ef983f8d
Moussa kebe <kebemoussa19@yahoo.fr>
2024-12-05 13:42:23 +0100

    ajout boutons fermer et règle

--- js/script.js
+++ js/script.js
@@ -14,3 +14,22 @@
 //   temps = temps <= 0 ? 0 : temps - 1
 // }, 1000)
 
+let button = document.getElementById('retourBouton');
+
+button.addEventListener('click', function () {
+    window.location.href = 'index.html'; 
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+    const howToPlay = document.getElementById('howToPlay');
+    const lireLesRegles = document.getElementById('lireLesRegles');
+    const closeButton = document.getElementById('closeButton');
+    lireLesRegles.addEventListener('click', () => {
+        howToPlay.style.display = 'block';
+
+    });
+
+    closeButton.addEventListener('click', () => {
+        howToPlay.style.display = 'none';
+    });
+});
